b482c62b029052537c3b181de3d972a547c2df44
Avei20 <muhammadabdulazizalghofari@gmail.com>
2022-10-10 14:16:35 +0700

    First Commit with Windows Config

---
+++ main.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+import os
+from selenium.webdriver.common import keys
+
+# driver = webdriver.Firefox(r'./webdriver/geckodriver-win.exe')
+print(os.path.join('./webdriver/geckodriver-win.exe'))
+
+url = 'https://forms.gle/NXixKVX3uYfwQ4wm6'
+
+# driver.get(url)
+
