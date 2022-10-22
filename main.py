6c296b63560444054054592379d03271183475af
avei <muhammadabdulazizialghofari@gmail.com>
2022-10-22 17:21:23 +0700

    Adding LLinux Driver

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 import os
 from selenium.webdriver.common import keys
 
-# driver = webdriver.Firefox(r'./webdriver/geckodriver-win.exe')
+driver = webdriver.Firefox('./webdriver/geckodriver-linux')
 print(os.path.join('./webdriver/geckodriver-win.exe'))
 
 url = 'https://forms.gle/NXixKVX3uYfwQ4wm6'
